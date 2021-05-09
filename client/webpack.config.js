1cf6690319ccc2f4319f253ac82402d362e34788
Piotr Rasztabiga <piotr.rasztabiga@radpoint.pl>
2021-05-09 13:11:22 +0200

    fix: remove node-sass

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,10 +17,6 @@ const config = {
                 test: /\.css$/,
                 use: ["style-loader", "css-loader"],
             },
-            {
-                test: /\.scss$/,
-                use: ["style-loader", "css-loader", "sass-loader"],
-            },
         ],
     },
     resolve: {
